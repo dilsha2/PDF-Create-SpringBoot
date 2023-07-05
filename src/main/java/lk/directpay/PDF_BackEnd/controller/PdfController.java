b23e6673d37e366eb1903bb45c7098116c54b042
unknown <diliprasanna96@gmail.com>
2023-07-05 17:06:43 +0530

    create service package and class

--- src/main/java/lk/directpay/PDF_BackEnd/controller/PdfController.java
+++ src/main/java/lk/directpay/PDF_BackEnd/controller/PdfController.java
@@ -1,4 +1,15 @@
 package lk.directpay.PDF_BackEnd.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/pdf")
 public class PdfController {
+
+    @GetMapping
+    public String getPdf(){
+        return "pdf";
+    }
 }
--- src/main/java/lk/directpay/PDF_BackEnd/service/PdfService.java
+++ src/main/java/lk/directpay/PDF_BackEnd/service/PdfService.java
@@ -1,4 +1,26 @@
 package lk.directpay.PDF_BackEnd.service;
 
+import com.itextpdf.text.*;
+import com.itextpdf.text.pdf.PdfWriter;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+import java.io.ByteArrayOutputStream;
+
+@Service
 public class PdfService {
+
+    public ResponseEntity<String> create() throws DocumentException {
+
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        Document document = new Document(PageSize.A4);
+        PdfWriter.getInstance(document,outputStream);
+        document.open();
+
+        Paragraph paragraph = new Paragraph("Text Document");
+        paragraph.setAlignment(Element.ALIGN_CENTER);
+        document.add(paragraph);
+
+        return null;
+    }
 }
